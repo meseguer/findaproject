7c11f32d2fded37c0ad9735090245580a256df35
Ruben Meseguer <rubenmeseguer7@gmail.com>
2019-05-12 17:12:01 +0200

    Test model built

--- backend/model.py
+++ backend/model.py
@@ -33,10 +33,27 @@ for w in filtered_sent:
 
 # Classify 
 
+# Import pandas
+import pandas as pd
+data=pd.read_csv('/mnt/c/xampp/htdocs/findaproject/backend/train.tsv', sep='\t')
+
 # Feature Generation using Bag of Words
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.tokenize import RegexpTokenizer
 #tokenizer to remove unwanted elements from out data like symbols and numbers
 token = RegexpTokenizer(r'[a-zA-Z0-9]+')
 cv = CountVectorizer(lowercase=True,stop_words='english',ngram_range = (1,1),tokenizer = token.tokenize)
-text_counts= cv.fit_transform(stemmed_words)
+text_counts= cv.fit_transform(data['Phrase'])
+
+# Split train and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(text_counts, data['Sentiment'], test_size=0.3, random_state=1)
+
+# Model building and evaluation
+from sklearn.naive_bayes import MultinomialNB
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
+# Model Generation Using Multinomial Naive Bayes
+clf = MultinomialNB().fit(X_train, y_train)
+predicted= clf.predict(X_test)
+print("MultinomialNB Accuracy:",metrics.accuracy_score(y_test, predicted))
