6181e3e55e2e5d2a3f0023fb1cd9f23cac1645de
Ruben Meseguer <rubenmeseguer7@gmail.com>
2019-06-02 14:18:44 +0200

    Styled components

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Link from 'next/link';
+import { withTheme } from '@material-ui/core/styles';
+import styled from 'styled-components';
 
 import Nav from './../../components/Nav';
 import ProjectList from './components/List';
@@ -22,99 +20,81 @@ const styles = theme => ({
   icon: {
     marginRight: theme.spacing.unit * 2
   },
-  heroUnit: {
-    backgroundColor: theme.palette.background.paper
-  },
-  heroContent: {
-    maxWidth: 600,
-    margin: '0 auto',
-    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`
-  },
-  heroButtons: {
-    marginTop: theme.spacing.unit * 4
-  },
-  layout: {
-    width: 'auto',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-      width: 1100,
-      marginLeft: 'auto',
-      marginRight: 'auto'
-    }
-  },
-  cardGrid: {
-    padding: `${theme.spacing.unit * 8}px 0`
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  cardMedia: {
-    paddingTop: '56.25%' // 16:9
-  },
-  cardContent: {
-    flexGrow: 1
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing.unit * 6
-  }
+  heroContent: {},
+  heroButtons: {}
 });
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+class Homepage extends React.Component {
+  state = {
+    projects: []
+  };
 
-function Album(props) {
-  const { classes } = props;
+  async getInitialProps() {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+    this.setState({ projects: data.projects });
+  }
 
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      {/* <Nav /> */}
-      <main>
-        {/* Hero unit */}
-        <div className={classes.heroUnit}>
-          <div className={classes.heroContent}>
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="textPrimary"
-              gutterBottom
-            >
-              Find a project to collaborate on
-            </Typography>
-            <Typography
-              variant="h6"
-              align="center"
-              color="textSecondary"
-              paragraph
-            >
-              Find your next big project to work on. Sourced from all over the
-              web, including reddit, discord, facebook and more!
-            </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={16} justify="center">
-                <Grid item>
-                  <Link href="/about">
-                    <Button variant="contained" color="primary">
-                      Get started
-                    </Button>
-                  </Link>
+  render() {
+    const { theme } = this.props;
+    const { projects } = this.state;
+
+    const HeroWrapper = styled.div`
+      background-color: ${theme.palette.background.paper};
+    `;
+
+    const HeroContent = styled.div`
+      max-width: 600px;
+      margin: 0 auto;
+      padding: ${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px;
+    `;
+
+    const HeroButtons = styled.div`
+      margin-top: ${theme.spacing.unit * 4};
+    `;
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <main>
+          <HeroWrapper>
+            <HeroContent>
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="textPrimary"
+                gutterBottom
+              >
+                Find a project to collaborate on
+              </Typography>
+              <Typography
+                variant="h6"
+                align="center"
+                color="textSecondary"
+                paragraph
+              >
+                Find your next big project to work on. Sourced from all over the
+                web, including reddit, discord, facebook and more!
+              </Typography>
+              <HeroButtons>
+                <Grid container spacing={16} justify="center">
+                  <Grid item>
+                    <Link href="/about">
+                      <Button variant="contained" color="primary">
+                        Get started
+                      </Button>
+                    </Link>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </div>
-          </div>
-        </div>
-        <ProjectList projects={cards} />
-      </main>
-    </React.Fragment>
-  );
+              </HeroButtons>
+            </HeroContent>
+          </HeroWrapper>
+          <ProjectList projects={projects} />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
-Album.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(Album);
+export default withTheme()(Homepage);
